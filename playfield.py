c3a3c52d30b644901c2248a7852b2d9a44ecea2b
David Britton <bubb@Bubbs-Air.hsd1.ca.comcast.net>
2022-04-06 14:17:02 -0700

    Made changes to game settings and display messages.

--- playfield.py
+++ playfield.py
@@ -1,5 +1,6 @@
 from computer import computer
 from humans import human
+from player import Player
 
 class Playfield:
     def __init__(self):
@@ -8,15 +9,52 @@ class Playfield:
 
     def run_game(self):
         self.display_message()
+        self.game_settings()
         self.battlefield()
         self.display_winner()
 
     def display_message(self):
-        print()
+        print("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
+        print("""RULES: Rock crushes Scissors
+                        Scissors cuts Paper 
+                        Paper covers Rock 
+                        Rock crushes Lizard 
+                        Lizard poisons Spock 
+                        Spock smashes Scissors 
+                        Scissors decapitates Lizard 
+                        Lizard eats Paper 
+                        Paper disproves Spock 
+                        Spock vaporizes Rock""")
+
+    def game_settings(self):
+        game_type = input(
+            'Do you want to play single player or multiplayer?: ')
+        game_settings = False
+        while game_settings == False:
+            if game_type == 'single player':
+                self.player_two = computer()
+                self.player_one.player_name()
+                self.player_two.computer_name()
+                print('Are you really sure you want to play against the AI?')
+                game_settings = True
+            elif game_type == 'multiplayer':
+                self.player_two = human()
+                self.player_one.player_one_name()
+                self.player_two.player_two_name()
+                print('Awesome! Let the games begin!')
+                game_settings = True
+
 # displays welcome message and rules of the game
 # 
-    def battlefield(self):
-        print()
+    def battlefield(self, Player):
+        players = human, computer
+        gestures = ['Rock', 'Paper','Scissors', 'Lizard','Spock']
+
+        if human == input("Rock"):
+            
+
+            print()
+
 # shows player one and player two
 # RPLSP playfield, displays list of gestures, lets user/AI choose gesture
 # if user chooses something other than list of choices, user will be asked to pick from list of choices
